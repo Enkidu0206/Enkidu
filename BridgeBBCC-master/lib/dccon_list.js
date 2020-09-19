47ab650b58edcd964c5adf8503d7a3f8614650cc
Enkidu0206 <yhh011721@gmail.com>
2020-09-19 18:18:41 +0900

    Initial commit

---
+++ BridgeBBCC-master/lib/dccon_list.js
@@ -0,0 +1,21 @@
+﻿dcConsData = [
+	{name:"구다.gif",	keywords:["구다", "구다임"],	tags:["페", "fgo"]},
+	{name:"나쁜문명.png",	keywords:["나쁜문명", "분쇄"],	tags:["페", "fgo"]},
+	{name:"우효.png",	keywords:["우효", "우효~"],	tags:["페", "fgo"]},
+	{name:"좋은문명.png",	keywords:["좋은문명", "좋음"],	tags:["페", "fgo"]},
+	{name:"가즈아아.png",	keywords:["가즈아", "구다 가즈아"],	tags:["페", "fgo"]},
+	{name:"ㄴㄷㅆ.png",	keywords:["ㄴㄷㅆ", "ㅆㄷ"],	tags:["클", "철수"]},
+	{name:"심판의 시간.png",	keywords:["심판", "심시"],	tags:["클", "철수"]},
+	{name:"이건사기.png",	keywords:["철수사기", "사기"],	tags:["클", "철수"]},
+	{name:"인생을 살아라.png",	keywords:["철수인살", "인생"],	tags:["클", "철수"]},
+	{name:"잘했다.png",	keywords:["철수칭찬", "칭찬"],	tags:["클", "철수"]},
+	{name:"총먹.png",	keywords:["철수총먹", "총먹"],	tags:["클", "철수"]},
+	{name:"클바.png",	keywords:["철수바", "클바"],	tags:["클", "철수"]},
+	{name:"클하.png",	keywords:["철수하", "클하"],	tags:["클", "철수"]},
+	{name:"은하 개쩜!.png",	keywords:["은하쩔어", "쩔어"],	tags:["클", "은하"]},
+	{name:"은하 아말.png",	keywords:["은하아말", "아말"],	tags:["클", "은하"]},
+	{name:"은하 쪼아.png",	keywords:["은하쪼아", "쪼아"],	tags:["클", "은하"]},
+	{name:"은하귀.png",	keywords:["은하귀찮", "귀찮"],	tags:["클", "은하"]},
+	{name:"나유타 뿌우.png",	keywords:["나유타 뿌우", "뿌우"],	tags:["나유타", "아르고나비스"]}
+
+];
